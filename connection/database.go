8dd46db7c801c38a6e4a8b703be17443fef6a048
finestarix <irenaldyleonarto@gmail.com>
2019-12-28 20:09:31 +0700

    Connection to postgres Success

---
+++ connection/database.go
@@ -0,0 +1,46 @@
+package connection
+
+import (
+	"fmt"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+	//"github.com/lib/pq"
+	"github.com/joho/godotenv"
+	"log"
+	"os"
+)
+
+var databaseName, databasePassword, databaseUser, databaseHost, databasePort, databaseSSL, databaseType string
+var connectionString string
+
+func init() {
+
+	err := godotenv.Load()
+
+	if err != nil {
+		log.Print("Failed to Load .env File")
+		os.Exit(3)
+	}
+
+	databaseName = os.Getenv("databaseName")
+	databasePassword = os.Getenv("databasePassword")
+	databaseUser = os.Getenv("databaseUser")
+	databaseHost = os.Getenv("databaseHost")
+	databasePort = os.Getenv("databasePort")
+	databaseSSL = os.Getenv("databaseSSL")
+
+	databaseType = os.Getenv("databaseType")
+
+	connectionString =  "dbname=" + databaseName + " " +
+					    "password=" + databasePassword + " " +
+						"user=" + databaseUser + " " +
+						"host=" + databaseHost + " " +
+						"port=" + databasePort + " " +
+						"sslmode=" + databaseSSL
+
+	fmt.Println(connectionString)
+}
+
+func ConnectDatabase() (*gorm.DB, error) {
+	return gorm.Open(databaseType, connectionString)
+}
---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+func main() {
+
+	//schema, err := graphql.NewSchema(graphql.SchemaConfig{
+	//	Query:        nil,
+	//	Mutation:     nil,
+	//})
+	//
+	//if err != nil {
+	//	panic(err.Error())
+	//}
+
+}
